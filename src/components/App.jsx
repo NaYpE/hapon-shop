8475af12b19fb9ed72c096e1d1cb1207c2180cb0
Angel Castro <angel_c_g@hotmail.com>
2021-10-22 00:17:40 -0500

    copie y pegue elementos de la pagina estatica

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Layout from "../containers/Layout";
+import Login from "../containers/Login";
+import '../styles/hapon-styles.css';
 
 const App = () => {
     return (
-        <h1>Hola Mundo!</h1>
+        <Layout>
+            <Login />
+        </Layout>
     );
 }
 
