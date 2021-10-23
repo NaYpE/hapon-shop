7b65aa4e4a2bbc8965ca147b160f08889d39c916
Angel Castro <angel_c_g@hotmail.com>
2021-10-22 21:18:22 -0500

    Login, not found, create new password, package actualizado

--- src/containers/CreateNewPasswrod.jsx
+++ src/containers/CreateNewPasswrod.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import '../styles/Login.scss';
 
-const Login = () => {
+const CreateNewPasword = () => {
     return (
         <div className="login">
             <div className="form-container">
                 <img src="../imagenes/hapon-logo.png" alt="logo" className="logo" />
 
-                <h1 className="title">¡Bienvenido a Hapon!</h1>
-                <p className="subtitle">Ingresa usuario y contraseña</p>
+                <h1 className="title">Crear una nueva contraseña</h1>
+                <p className="subtitle">Ingresa una nueva contraseña para tu cuenta</p>
 
                 <form action="/" className="form">
-                    <label for="password" className="label">Usuario</label>
-                    <input type="password" id="password" placeholder="NaYpE^" className="input input-password" />
+                    <label for="password" className="label">Contraseña</label>
+                    <input type="password" id="password" placeholder="*********" className="input input-password" />
 
-                    <label for="new-password" className="label">Contraseña</label>
+                    <label for="new-password" className="label">Confirmar Contraseña</label>
                     <input type="password" id="new-password" placeholder="*********" className="input input-password" />
 
                     <input type="submit" value="Enviar" className="primary-button login-button" />
@@ -24,4 +24,4 @@ const Login = () => {
     );
 }
 
-export default Login;
+export default CreateNewPasword;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './components/App.jsx';
+import App from './routes/App.jsx';
 
 const container = document.getElementById('app');
+
 ReactDOM.render(<App />, container);
